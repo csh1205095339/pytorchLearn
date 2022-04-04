67ce1a276645a5fca9e49d93b3958fad8540b15e
csh1205095339 <1205095339@qq.com>
2022-04-05 01:04:35 +0800

    deep learning start

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+import torch
+import torchvision
+from matplotlib import pyplot as plt
+import numpy as np
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x = np.arange(0, 6, 0.1)
+    print(x)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+    plt.plot(x, y1, label="sin")
+    plt.plot(x, y2, linestyle="--", label="cos")  # 用虚线绘制
+    plt.xlabel("x")  # x轴标签
+    plt.ylabel("y")  # y轴标签
+    plt.title('sin & cos')  # 标题
+    plt.legend()
+    plt.show()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
